51f8050f253b9f1060cc1788438e0bf4a84c445d
Tiur Mawaddah <tiurzm@gmail.com>
2020-02-05 11:16:15 -0600

    working on home page

---
+++ client/src/components/Wrapper/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css"
+import logo1 from './cn.jpeg';
+import logo2 from './mrn.jpeg';
+import logo3 from './gr.jpeg';
+
+
+function Wrapper(props) {
+  return (
+    // <main className="wrapper" {...props}></main>
+    <>
+      <div id="carouselWrapperFade" className="carousel slide carousel-fade" data-ride="carousel" {...props}>
+      {props.children}
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={logo1} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={logo2} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={logo3} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselWrapperFade" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselWrapperFade" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a> 
+      </div>
+    </>
+  );
+}
+
+export default Wrapper;
