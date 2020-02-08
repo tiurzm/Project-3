8b882e561dbc34ff1bd3358804ffd17eeb741ced
Tiur Mawaddah <tiurzm@gmail.com>
2020-02-08 11:12:44 -0600

    made new pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import Carousel from "./components/Carousel"
 import { Container } from "./components/Grid"
 import Home from "./pages/Home";
-// import Profile from "./pages/Profile";
-// import "./App.css"
-
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App (){
   
     return (
+      <Router>
+
       <Container fluid>
         <Navbar/>
         <Carousel/>
-        <Home />
-        {/* <Profile/> */}
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
+        </Switch>
       </Container>
+      </Router>
+
     );
 }
 
