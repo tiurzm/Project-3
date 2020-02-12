6d735c7fe17fb8b786dba1b2267226edb35af114
Tiur Mawaddah <tiurzm@gmail.com>
2020-02-12 11:02:31 -0600

    fixing navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,96 +1,89 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css"
 
-function LoginButton(props) {
-    return (
-      <button onClick={props.onClick}>
-        Login
-      </button>
-    );
-}
-  
-function LogoutButton(props) {
-    return (
-      <button onClick={props.onClick}>
-        Logout
-      </button>
-    );
-}
+// function LoginButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Login
+//       </button>
+//   );
+// }
+
+// function LogoutButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Logout
+//       </button>
+//   );
+// }
 
 class Navbar extends React.Component {
-    state = {
-        isTop: true,
-        isLoggedin: false,
-      };
+  state = {
+    isTop: true
+    // ,
+    // isLoggedin: false,
+  };
 
-      handleLoginClick() {
-        this.setState({isLoggedIn: true});
+  // handleLoginClick() {
+  //   this.setState({ isLoggedIn: true });
+  // }
+
+  // handleLogoutClick() {
+  //   this.setState({ isLoggedIn: false });
+  // }
+
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 30;
+      if (isTop !== this.state.isTop) {
+        this.setState({ isTop })
       }
-    
-      handleLogoutClick() {
-        this.setState({isLoggedIn: false});
-      }
-      
-      componentDidMount() {
-        document.addEventListener('scroll', () => {
-          const isTop = window.scrollY < 30;
-          if (isTop !== this.state.isTop) {
-            this.setState({ isTop })
-          }
-        });
-      }
-    
-    render() {
+    });
+  }
+
+  render() {
 
     let navbar = "navbar navbar-expand-lg fixed-top py-0";
-    if(this.state.isTop) {
-        navbar += " navbar-dark bg-transparent"
+    if (this.state.isTop) {
+      navbar += " navbar-dark bg-transparent"
     } else {
-        navbar += " navbar-light bg-white"
+      navbar += " navbar-light bg-white"
     }
 
-    let style = "btn pl-0 px-lg-3";
-    if(!this.state.isTop) {
-        style += " text-dark"
-    } else {
-        style += " text-white"
-    }
+    // const isLoggedIn = this.state.isLoggedIn;
+    // let button;
 
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
+    // if (isLoggedIn) {
+    //   button = <LogoutButton onClick={this.handleLogoutClick} />;
+    // } else {
+    //   button = <LoginButton onClick={this.handleLoginClick} />;
+    // }
 
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
-    
     return (
-        <>
-            <nav className={navbar} >
-                <a className="navbar-brand" href="/">GroupAway</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse float-right" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/register">Register<span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item active" data-toggle="modal" data-target="#loginModal">
-                            <button type="button" 
-                            className={style} 
-                            isLoggedIn={isLoggedIn}>{button}
-                            </button>
-                            {/* condition renderin one line if statement */}
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </>
+      <nav className={navbar} >
+        <Link className="navbar-brand" to="/">
+          GroupAway
+          </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse float-right" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/register"
+                className={window.location.pathname === "/register" ? "nav-link active" : "nav-link"}>
+                Register
+                </Link>
+            </li>
+            <li className="nav-item" >
+              <a className="nav-link pointer" data-toggle="modal" data-target="#loginModal">Log In</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
-    }
+  }
 }
 
 export default Navbar;
