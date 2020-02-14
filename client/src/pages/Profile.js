a8b5f6e9f1936d9572c1acd2784495e39ef225bc
JLG <mjgaumnitz@kc.rr.com>
2020-02-13 21:31:28 -0600

    working on chat code

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,44 @@
-import React from "react";
-import Calender from "../components/calendar";
+import React, { Component }  from "react";
+import Calendar from "../components/calendar";
 import { Container, Row, Col } from "../components/Grid";
 // import Navbar from "../components/Navbar";
+import UsernameForm from './components/chat/UsernameForm'
+import ChatScreen from '../ChatScreen'
 
-function Profile () {
+class Profile extends Component {
+    constructor() {
+        super()
+        this.state = {
+          currentUsername: '',
+          currentScreen: 'WhatIsYourUsernameScreen'
+        }
+        this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
+      }
+      onUsernameSubmitted(username) {
+        fetch('http://localhost:3001/users', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username }),
+        })
+          .then(response => {
+            this.setState({
+              currentUsername: username,
+              currentScreen: 'ChatScreen'
+            })
+          })
+          .catch(error => console.error('error', error))
+      }
+    
+    render() {
+    if (this.state.currentScreen === 'WhatIsYourUsernameScreen') {
+      return <UsernameForm onSubmit={this.onUsernameSubmitted} />
+    }
+    if (this.state.currentScreen === 'ChatScreen') {
+      return <ChatScreen currentUsername={this.state.currentUsername} />
+    }
+
     return(
         <>
         {/* <Navbar/> */}
@@ -17,9 +52,11 @@ function Profile () {
 
                 </Col>
             </Row>
-            <Calender/>
+            <Calendar/>
         </Container>
         </>
     );
+
+    }
 }
 export default Profile;
