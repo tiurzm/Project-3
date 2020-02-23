ccfb0b0b35bad913d4a5886260a4e829db397a44
Tiur Mawaddah <tiurzm@gmail.com>
2020-02-23 13:01:11 -0600

    editing trip's look

--- client/src/components/trips/index.js
+++ client/src/components/trips/index.js
@@ -7,7 +7,8 @@ class Trips extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      trips: []
+      trips: [], 
+      show: true
     };
 
     this.handleCalendarChange = this.handleCalendarChange.bind(this);
@@ -21,142 +22,79 @@ class Trips extends Component {
     });
   }
 
+  showTrip = () => {
+    if(this.state.show) {
+      this.setState({
+        show: false
+      })
+    } else {
+      this.setState({
+        show: true
+      })
+    }
+  }
+
   handleCalendarChange(didItChange) {
     axios.getTrips().then(resp => {
-        let tripData = resp.data[0].trip;
-  
-        this.setState({ 
-            trips: tripData,
-        });
+      console.log(resp)
+      let tripData = resp.data[0].trip;
+
+      this.setState({
+        trips: tripData,
       });
+    });
   }
 
   render() {
     return (
-      <div>
-        <div class="accordion" id="accordionExample">
-          <div class="card">
-            <div class="card-header" id="headingOne">
-              <h2 class="mb-0">
-                <button
-                  class="btn btn-link"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#collapseOne"
-                  aria-expanded="true"
-                  aria-controls="collapseOne"
-                >
-                  Collapsible Group Item #1
-                </button>
-              </h2>
-            </div>
 
-            <div
-              id="collapseOne"
-              class="collapse show"
-              aria-labelledby="headingOne"
-              data-parent="#accordionExample"
-            >
-              <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                non cupidatat skateboard dolor brunch. Food truck quinoa
-                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably
-                haven't heard of them accusamus labore sustainable VHS.
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-header" id="headingTwo">
-              <h2 class="mb-0">
-                <button
-                  class="btn btn-link collapsed"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="collapseTwo"
-                >
-                  Collapsible Group Item #2
-                </button>
-              </h2>
-            </div>
-            <div
-              id="collapseTwo"
-              class="collapse"
-              aria-labelledby="headingTwo"
-              data-parent="#accordionExample"
-            >
-              <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                non cupidatat skateboard dolor brunch. Food truck quinoa
-                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably
-                haven't heard of them accusamus labore sustainable VHS.
-              </div>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-header" id="headingThree">
-              <h2 class="mb-0">
-                <button
-                  class="btn btn-link collapsed"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#collapseThree"
-                  aria-expanded="false"
-                  aria-controls="collapseThree"
-                >
-                  Collapsible Group Item #3
-                </button>
-              </h2>
-            </div>
-            <div
-              id="collapseThree"
-              class="collapse"
-              aria-labelledby="headingThree"
-              data-parent="#accordionExample"
-            >
-              <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                non cupidatat skateboard dolor brunch. Food truck quinoa
-                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua
-                put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                excepteur butcher vice lomo. Leggings occaecat craft beer
-                farm-to-table, raw denim aesthetic synth nesciunt you probably
-                haven't heard of them accusamus labore sustainable VHS.
-              </div>
-            </div>
-          </div>
-        </div>
-        {this.state.trips.map(trip => {
+      <div>
+        {/* {this.state.trips.map(trip => {
           return (
             <p>
-              Trip Title: {trip.title} <br />
+              Trip Title: {trip.title} <br /> */}
               {/* Trip ID: {trip._id}<br/> */}
               {/* Trip User: {trip.user}<br/> */}
-              Guests: {trip.guests}
+              {/* Guests: {trip.guests}
               <br />
               Trip Start <Clock deadline={trip.start} />
-              <br />
+              <br /> */}
               {/* Trip End {trip.end}<br/> */}
-              Description {trip.description}
+              {/* Description {trip.description}
             </p>
           );
-        })}
+        })} */}
+
+        <div className="accordion" id="trip">
+          {this.state.trips.map(trip => {
+            console.log(trip)
+            return (
+              <div className="card">
+                <div className="card-header" id={trip._id}>
+                  <h2 className="mb-0" onClick= {this.showTrip}>
+                    <button
+                      className="btn btn-link"
+                      type="button" 
+                    >
+                      {trip.title}
+                    </button>
+                  </h2>
+                </div>
+
+                <div
+                  id="a-trip"
+                >
+                  <div className="card-body">
+                    Guests: {trip.guests} <br />
+                    Trip Start: <Clock deadline={trip.start} /> <br />
+                    Description: {trip.description}
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+
         <Calender detectChange={this.handleCalendarChange} />
       </div>
     );
