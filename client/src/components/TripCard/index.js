e5713f7f603f6ddc0f83cf7573ea053761d5bbd3
Tiur Mawaddah <tiurzm@gmail.com>
2020-02-24 13:17:44 -0600

    working on diplay trip's information on trip's card

--- client/src/components/TripCard/index.js
+++ client/src/components/TripCard/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import "./style.css"
 
 
-export default function TripCard(props) {
-    console.log(props)
+class TripCard extends Component {
+    constructor(props) {
+        super(props);
+      }
+    render(){
     return (
-        <Modal show={props.show} id="trip">
+        <Modal show={this.props.show} id="trip">
         <Modal.Dialog>
-            <Modal.Header closeButton onClick={props.close}>
+            <Modal.Header closeButton onClick={this.props.close}>
+                Trip's Information
             </Modal.Header>
             <Modal.Body>
-            <div className="card" {...props}>
+                <Button onClick={this.props.delete}
+                variant="danger"
+                >
+                    Delete
+                </Button>
+            {/* <div className="card" {...props}>
                 <h5 className="card-header">Trip</h5>
                 <div className="card-body">
                     <h5 className="card-title">
@@ -31,9 +40,12 @@ export default function TripCard(props) {
                     <Button onClick={props.delete}
                         variant="danger">Delete</Button>
                 </div>
-                </div>
+                </div> */}
             </Modal.Body>
         </Modal.Dialog>
         </Modal>
     )
+    }
 }
+
+export default TripCard;
