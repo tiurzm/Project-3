008ada8da6429cc6b5b74478b7025aae7916ba6d
JLG <mjgaumnitz@kc.rr.com>
2020-02-25 21:08:11 -0600

    added button to location page & removed map from home page

--- client/src/pages/Location.js
+++ client/src/pages/Location.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 import Map from "../components/GoogleMaps";
+import { Link } from "react-router-dom";
+
 
 function Location() {
     const googleKey = process.env.REACT_APP_GOOGLE_KEY
     return (
-        <Container style={{marginTop: "150px", marginBottom: "150px"}}>
+        <Container style={{ marginTop: "150px", marginBottom: "150px" }}>
             <Row>
+                <Col>
+                    <Link to="/profile">
+                        <button className="btn btn-info mb-2 ">
+                            Back to Calendar <i className="far fa-calendar-alt"></i>
+                        </button>
+                    </Link>
+
+                </Col>
                 <Col data-aos="fade-up" className="mb-5 mx-auto">
                     <div style={{ width: '80vw', height: '80vh' }}>
                         <Map
