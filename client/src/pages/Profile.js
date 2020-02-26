f42e7dedf4831274dce53f2819bc645143e4ffe9
Tiur Mawaddah <tiurzm@gmail.com>
2020-02-26 15:24:26 -0600

    added font-icon

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -29,7 +29,7 @@ class Profile extends Component {
         <Container style={{ marginTop: "150px", marginBottom: "150px" }}>
           <Row>
             <Col className="col-md-8 text-justify">
-              <p>Hello {this.props.username}</p>
+              <p>Welcome back, {this.props.username}!</p>
               <p>
                 Instructions here: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
               </p>
