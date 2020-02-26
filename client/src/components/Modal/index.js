a6fbdb170d54efe0ea5074cbac6df1b424de043c
Tiur Mawaddah <tiurzm@gmail.com>
2020-02-26 15:26:57 -0600

    added font-icon

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -8,7 +8,7 @@ function Modal(props) {
           <div className="modal-content">
             <div className="modal-header">
               <h4 className="modal-title mx-auto text-center" id="loginLabel"><i className="fas fa-luggage-cart"></i> Welcome</h4>
-              <button type="button" className="close ml-0 pl-0" data-dismiss="modal" aria-label="Close">
+              <button type="button" className="close ml-0 px-2" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
